716636d9f28bce129660e21e35b49f0e1d69e5b4
Kashif Sheikh <kashifshaikh810>
2022-10-27 17:52:39 +0500

    Loader

--- TourApp/src/screens/Home/HomeMarkup.js
+++ TourApp/src/screens/Home/HomeMarkup.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, FlatList, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 import Header from '../../components/Header/Header';
 import styles from './styles';
 import LikeAndDislikeIcon from 'react-native-vector-icons/AntDesign';
@@ -14,42 +21,46 @@ const HomeMarkup = props => {
         <Text style={styles.tourHeading}>Tours</Text>
       </View>
 
-      <FlatList
-        data={props.tours && props.tours}
-        renderItem={({item, index}) => (
-          <View style={styles.card}>
-            <Image source={{uri: item.imageFile}} style={styles.tourImage} />
-            <View style={styles.tagsContainer}>
-              {item.tags &&
-                item.tags.map(tag => {
-                  return (
-                    <TouchableOpacity>
-                      <Text style={styles.tagText}>#{tag}</Text>
-                    </TouchableOpacity>
-                  );
-                })}
+      {props?.loading ? (
+        <ActivityIndicator size={30} color="gray" />
+      ) : (
+        <FlatList
+          data={props.tours && props.tours}
+          renderItem={({item, index}) => (
+            <View style={styles.card}>
+              <Image source={{uri: item.imageFile}} style={styles.tourImage} />
+              <View style={styles.tagsContainer}>
+                {item.tags &&
+                  item.tags.map((tag, index) => {
+                    return (
+                      <TouchableOpacity key={index}>
+                        <Text style={styles.tagText}>#{tag}</Text>
+                      </TouchableOpacity>
+                    );
+                  })}
 
-              <TouchableOpacity style={styles.likeContainer}>
-                <LikeAndDislikeIcon name="like2" size={20} color="blue" />
-                <Text style={styles.like}>2</Text>
-                <Text style={styles.like}>Likes</Text>
-              </TouchableOpacity>
-            </View>
-
-            <View style={styles.titleAndDescriptionContainer}>
-              <Text style={styles.title}>{item.title}</Text>
-              <View style={styles.flexRow}>
-                <Text numberOfLines={2} style={styles.description}>
-                  {item.description}
-                </Text>
-                <TouchableOpacity onPress={() => {}}>
-                  <Text style={styles.readMore}>Read More</Text>
+                <TouchableOpacity style={styles.likeContainer}>
+                  <LikeAndDislikeIcon name="like2" size={20} color="blue" />
+                  <Text style={styles.like}>2</Text>
+                  <Text style={styles.like}>Likes</Text>
                 </TouchableOpacity>
               </View>
+
+              <View style={styles.titleAndDescriptionContainer}>
+                <Text style={styles.title}>{item.title}</Text>
+                <View style={styles.flexRow}>
+                  <Text numberOfLines={2} style={styles.description}>
+                    {item.description}
+                  </Text>
+                  <TouchableOpacity onPress={() => {}}>
+                    <Text style={styles.readMore}>Read More</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
-          </View>
-        )}
-      />
+          )}
+        />
+      )}
     </View>
   );
 };
