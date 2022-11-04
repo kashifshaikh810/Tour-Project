e3d35ea9cf0dfafa087a7568e4b0e564f6937bba
Kashif Sheikh <kashifshaikh810>
2022-11-04 16:08:34 +0500

    Implement search screen UI with dynamic tour data

--- TourApp/src/screens/SearchScreen/SearchScreenMarkup.js
+++ TourApp/src/screens/SearchScreen/SearchScreenMarkup.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+  Image,
+} from 'react-native';
 import styles from './styles';
 import Header from '../../components/Header/Header';
+import SearchIcon from 'react-native-vector-icons/FontAwesome';
+import LikeAndDislikeIcon from 'react-native-vector-icons/AntDesign';
 
 const SearchScreenMarkup = props => {
   return (
@@ -11,6 +21,114 @@ const SearchScreenMarkup = props => {
       <View style={styles.searchHeading}>
         <Text style={styles.search}>Search Tour</Text>
       </View>
+
+      <View>
+        <View style={styles.inputAndIconContainer}>
+          <TextInput
+            placeholder="Search"
+            keyboardType="default"
+            style={[
+              styles.textInput,
+              props?.searchError !== '' && styles.borderRedColor,
+            ]}
+            placeholderTextColor={
+              props?.searchError.length > 1 ? 'red' : 'gray'
+            }
+            value={props?.search}
+            onChangeText={text => props?.searchOnChange(text)}
+          />
+          <TouchableOpacity onPress={() => props?.searchOnPressHandler()}>
+            <SearchIcon name="search" size={35} style={styles.icon} />
+          </TouchableOpacity>
+        </View>
+        {props?.searchError && (
+          <Text
+            style={[
+              styles.errorText,
+              props?.searchError && {marginBottom: 10},
+            ]}>
+            {props?.searchError}
+          </Text>
+        )}
+      </View>
+
+      {props?.loading ? (
+        <ActivityIndicator size={30} color="gray" style={styles.loader} />
+      ) : (
+        <FlatList
+          data={props.tours && props.tours}
+          renderItem={({item, index}) => (
+            <View style={styles.card}>
+              <Image source={{uri: item?.imageFile}} style={styles.tourImage} />
+              <View style={styles.tagsContainer}>
+                {item.tags &&
+                  item.tags.map((tag, index) => {
+                    return (
+                      <TouchableOpacity
+                        key={index}
+                        style={styles.fix}
+                        onPress={() =>
+                          props.navigation.navigate('ToursByTag', {tag: tag})
+                        }>
+                        <Text style={styles.tagText}>#{tag}</Text>
+                      </TouchableOpacity>
+                    );
+                  })}
+              </View>
+              <TouchableOpacity
+                disabled={!props?.isAuthenticated}
+                style={styles.likeContainer}
+                onPress={() => props.likeOnPressHandler(item)}>
+                <LikeAndDislikeIcon
+                  name={
+                    item?.likes.find(like => like === props?.userId)
+                      ? 'like1'
+                      : 'like2'
+                  }
+                  size={20}
+                  color="blue"
+                />
+                {item.likes.length >= 2 ? (
+                  <Text>
+                    {' '}
+                    {`${
+                      item?.likes.find(like => like === props?.userId)
+                        ? 'You and'
+                        : ''
+                    } ${
+                      props?.isAuthenticated
+                        ? item.likes.length - 1
+                        : item.likes.length
+                    } ${item.likes.length >= 2 ? 'other people likes' : ''}  `}
+                  </Text>
+                ) : (
+                  <>
+                    <Text style={styles.like}>{item?.likes?.length}</Text>
+                    <Text style={styles.like}>
+                      {item?.likes?.length <= 1 ? 'Like' : 'Likes'}
+                    </Text>
+                  </>
+                )}
+              </TouchableOpacity>
+
+              <View style={styles.titleAndDescriptionContainer}>
+                <Text style={styles.title}>{item.title}</Text>
+                <View style={styles.flexRow}>
+                  <Text numberOfLines={2} style={styles.description}>
+                    {item.description}
+                  </Text>
+                  <TouchableOpacity
+                    onPress={() =>
+                      props.navigation.navigate('TourDetail', {id: item._id})
+                    }>
+                    <Text style={styles.readMore}>Read More</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
 };
--- TourApp/src/screens/SearchScreen/styles.js
+++ TourApp/src/screens/SearchScreen/styles.js
@@ -4,7 +4,25 @@ import tw from 'tailwind-react-native-classnames';
 const styles = StyleSheet.create({
   container: tw`flex-1 bg-white`,
   searchHeading: tw`w-full h-20 justify-center items-center`,
-  search: tw`text-xl text-gray-500 border-b-2 border-gray-200 w-10/12 text-center pb-2`,
+  search: tw`text-xl text-gray-500 border-b-2 border-gray-200 w-11/12 text-center pb-2`,
+  textInput: tw`w-10/12 border-2 border-gray-300 rounded self-center pl-4 pr-4 mt-1 text-gray-600`,
+  button: tw`w-11/12 h-11 bg-green-200 justify-center items-center rounded self-center mt-10 mb-5`,
+  borderRedColor: tw`border-red-500`,
+  inputAndIconContainer: tw`flex-row justify-center items-center mb-2`,
+  icon: tw`ml-2`,
+  errorText: tw`text-red-500 pl-5 pt-2`,
+  card: tw`border-2 border-gray-100 w-11/12 h-96 self-center pb-2 mt-5 mb-5`,
+  tourImage: tw`w-full h-40`,
+  tagsContainer: tw`flex-row mt-5`,
+  tagText: tw`text-base text-blue-600 pl-2`,
+  likeContainer: tw`flex-row flex-1 items-center ml-3 mb-5 mt-5`,
+  like: tw`ml-2 text-base text-blue-600`,
+  titleAndDescriptionContainer: tw`pl-2`,
+  title: tw`text-xl mt-2`,
+  description: tw`text-sm w-8/12 mt-2 mb-2 text-base`,
+  flexRow: tw`flex-row items-center`,
+  readMore: tw`text-blue-600 pt-6 text-base`,
+  loader: tw`mt-5`,
 });
 
 export default styles;
